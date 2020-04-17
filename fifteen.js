ca9315fad67d6005430fc7a87afb22892b0cbc6e
ursula95 <tatyana2101.kuznetsova@yandex.ru>
2020-04-17 22:59:48 +0700

    add drow function and styles for board

--- fifteen.js
+++ fifteen.js
@@ -24,7 +24,29 @@ function init() {
 }
 
 function drow() {
-    
+
+  const main = document.getElementById('main');
+  const boardBlock = document.createElement('div');
+  boardBlock.classList.add('board');  
+  main.append(boardBlock);
+
+  for(let row = 0; row < boardSize; row++) {
+    for(let column = 0; column < boardSize; column++) {
+      if(board[row][column] == 0) {
+        const emptyTile = document.createElement('div');
+        emptyTile.classList.add('emptyTile');
+        emptyTile.innerText = '0';
+        boardBlock.append(emptyTile);
+      } else {
+        const tile = document.createElement('div');
+        tile.classList.add('tile');
+        tile.innerText = board[row][column];
+        boardBlock.append(tile);
+      }
+    }
+  }
+
+  console.log(boardBlock);
 }
 
 function move() {
