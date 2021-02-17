dd9d60e97546c1670ad3c20ca810969064112a92
Ursula95 <tatyana2101.kuznetsova@yandex.ru>
2021-02-17 17:20:02 +0700

    remove errors files

--- bin/brain-games.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-import { sayHelloByName } from '../src/cli.js';
-
-console.log('Welcome to the Brain Games!');
-sayHelloByName();
--- src/cli.js
+++
@@ -1,8 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const sayHelloByName = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-};
-
-export { sayHelloByName };
